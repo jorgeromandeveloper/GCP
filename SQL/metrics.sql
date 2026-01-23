79ce23c35c6fcc1c81aac937e4e3435de674231c
jorgeromandeveloper <jorgeromandeveloper@gmail.com>
2026-01-23 13:52:04 +0100

    Added new metrics (closed issues and reopeneds one)

--- SQL/metrics.sql
+++ SQL/metrics.sql
@@ -6,6 +6,12 @@ FROM `project-e0c626dd-6b61-4b52-a57.analytics.issue_status_history`
 WHERE is_current = TRUE
   AND state = 'open';
 
+#Closed issues
+SELECT COUNT(*) AS open_issues
+FROM `project-e0c626dd-6b61-4b52-a57.analytics.issue_status_history`
+WHERE is_current = TRUE
+  AND state = 'closed';
+
 #Average time to close issues
 SELECT
   AVG(TIMESTAMP_DIFF(valid_to, valid_from, HOUR)) AS avg_hours_to_close
@@ -13,24 +19,31 @@ FROM `project-e0c626dd-6b61-4b52-a57.analytics.issue_status_history`
 WHERE state = 'open'
   AND valid_to IS NOT NULL;
 
+
 #Number of issues reopened
 
-WITH ordered_issues AS (
+WITH reopen_events AS (
   SELECT
     issue_id,
-    state,
-    ingestion_date,
-    LEAD(state) OVER (
+    valid_from,
+    LAG(state) OVER (
       PARTITION BY issue_id
-      ORDER BY ingestion_date
-    ) AS next_state
-  FROM `project-e0c626dd-6b61-4b52-a57.raw_data.github_issues`
+      ORDER BY valid_from
+    ) AS previous_state,
+    state
+  FROM `project-e0c626dd-6b61-4b52-a57.analytics.issue_status_history`
+),
+current_state AS (
+  SELECT issue_id
+  FROM `project-e0c626dd-6b61-4b52-a57.analytics.issue_status_history`
+  WHERE is_current = TRUE
+    AND state = 'open'
 )
 
-SELECT
-    issue_id,
-    state,
-    ingestion_date
-FROM ordered_issues
-WHERE
+SELECT DISTINCT r.issue_id
+FROM reopen_events r
+JOIN current_state c
+  ON r.issue_id = c.issue_id
+WHERE r.state = 'open'
+  AND r.previous_state = 'closed';
 
